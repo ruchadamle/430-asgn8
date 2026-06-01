0b24e52c17354ee5b07168a78f41fbe1232c7653
Rucha Damle <ruchadamle@email.com>
2026-06-01 14:20:12 -0700

    add data definitions

--- Asgn8.fs
+++ Asgn8.fs
@@ -1,1 +1,46 @@
-printfn "Hello, World!"
+// Definitions
+
+// Abstract syntax definitions
+type ExprC =
+    // float is closest thing to Racket's "Real" type
+    | NumC of float
+    | StrC of string
+    // No Symbol in F#, so string is used instead
+    | IdC of string
+    | IfC of ExprC * ExprC * ExprC
+    | LamC of string list * ExprC
+    | AppC of ExprC * ExprC list
+
+// Values and environment definitions
+type Value =
+    | NumV of float
+    | StrV of string
+    | BoolV of bool
+    | CloV of string list * ExprC * Env
+    | PrimV of string
+and Env = (string * Value) list
+
+// Representation of a top-level environment
+let topEnv : Env =
+    [
+        ("true", BoolV true)
+        ("false", BoolV false)
+        ("+", PrimV "+")
+        ("-", PrimV "-")
+        ("*", PrimV "*")
+        ("/", PrimV "/")
+        ("<=", PrimV "<=")
+        ("equal?", PrimV "equal?")
+        ("substring", PrimV "substring")
+        ("strlen", PrimV "strlen")
+        ("error", PrimV "error")
+    ]
+
+// Interpreter
+
+// Parser (ignored for now, since F# has no sexps)
+
+// Helpers
+
+
+// Tests
